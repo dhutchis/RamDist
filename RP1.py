cc6b0cc32454582c92228a67803aefcf645ef130
Dylan Hutchison <dhutchis@stevens.edu>
2013-12-31 09:19:31 -0500

    goodfraction_systematic works: {3: 0.25} (2^3 = 8, 2 good graphs out of 8)

--- RP1.py
+++ RP1.py
@@ -1,55 +1,72 @@
+from __future__ import division
 import igraph
-from future import division
+import csv
+import itertools
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 # terminology:  good graph has a K3 or an I4
 #               bad graph has neither a K3 nor an I4
 
 # Goal: for R(3,4)=9, determine the distribution of the proportion of good graphs of size n
 #       as n increases from 1 to 9
 def __main__():
-    list = create_dist(3,4)
-    print list
+    dic = {3:goodfraction_systematic(3,3,3)}
+    #dic = create_dist(3,3)
+    print dic
+    writer = csv.writer(open('dict.csv', 'wb'))
+    for key, value in dic.items():
+        writer.writerow([key, value])
 
 # Given q1, q2, create dict {1:0, 2:0, 3:0.05}
 # alternative? [(1,0),(2,0),(3,0.05),...]
 def create_dist(q1,q2):
-    print 'todo'
+    dic = dict()
+    for n in range(1,9):
+        dic[n] = goodfraction_systematic(n,q1,q2)
+        if dic[n] == 1 and (n == 1 or dic[n-1] == 1):
+            break
+    return dic
+    for n in itertools.count(9):
+        dic[n] = goodfraction_sample(n,q1,q2,10000) # fix sample size 10000
+        if dic[n] == 1 and dic[n-1] == 1:
+            break
+    return dic
 
 # Given n (<10), go through every graph of size n and count how many have are good and return the fraction
 def goodfraction_systematic(n, q1, q2):
-    if n < q1 and n < q2
+    if n < q1 and n < q2:
         return 0.0
-    if n >= 10
+    if n >= 10:
         print 'warning - this may take a while for n = ', n
-    g = igraph.Graph(n)
-    # clear graph
-    count_good = 0
-    count_bad = 0
-    while True:
-        break
+    #g = igraph.Graph(n) # empty graph
+    good = 0
+    bad = 0
+    for g in generate_graphs(n):
         # check graph - is it good?
         #  find max clique # mc
         mc = g.omega()
         if mc >= q1: # then good graph
             good += 1
+            logger.debug('n={}, {}, omega={}, good'.format(n,g,mc))
         else:
             #  find max independent set # mi
             mi = g.alpha()
             if mi >= q2: # then good graph
                 good += 1
+                logger.debug('n={}, {}, omega={}, alpha={} good'.format(n,g,mc,mi))
             else: #  bad graph!
                 bad += 1
-        # increment graph
-        igraph.
-
-        # if graph is the first graph, break
-
-    assert count_good + count_bad == 2 ** (n*(n-1)/2)
-    return count_good / count_bad
+                logger.debug('n={}, {}, omega={}, alpha={} BAD'.format(n,g,mc,mi))
+    assert good + bad == 2 ** (n*(n-1)/2)
+    return good / (good + bad)
 
 # edge 5-4 5-3 5-2 5-1
 
 def generate_graphs(n):
-    g = Graph(n)
+    g = igraph.Graph(n)
     for gg in generate_graphs_help(g,n-1,n-2):
         yield gg
     return
@@ -66,11 +83,12 @@ def generate_graphs_help(g,i,j):
         g.add_edge(i,j)
         for gg in generate_graphs_help(g,i,j-1):
             yield gg
-        g.delete_edges(i,j)
+        g.delete_edges((i,j))
     return
 
 # Given n and sample size m, sample m graphs of size n, count how many are good and return the fraction
-def goodfraction_sample(n,q1,q2,m)
+def goodfraction_sample(n,q1,q2,m):
     print 'todo'
 
 
+__main__()

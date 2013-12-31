c14de71b61bfb7ce9819dc42ab18669bac952505
Dylan Hutchison <dhutchis@stevens.edu>
2013-12-30 23:04:55 -0500

    sketched program skeleton, started implementing

---
+++ RP1.py
@@ -0,0 +1,76 @@
+import igraph
+from future import division
+# terminology:  good graph has a K3 or an I4
+#               bad graph has neither a K3 nor an I4
+
+# Goal: for R(3,4)=9, determine the distribution of the proportion of good graphs of size n
+#       as n increases from 1 to 9
+def __main__():
+    list = create_dist(3,4)
+    print list
+
+# Given q1, q2, create dict {1:0, 2:0, 3:0.05}
+# alternative? [(1,0),(2,0),(3,0.05),...]
+def create_dist(q1,q2):
+    print 'todo'
+
+# Given n (<10), go through every graph of size n and count how many have are good and return the fraction
+def goodfraction_systematic(n, q1, q2):
+    if n < q1 and n < q2
+        return 0.0
+    if n >= 10
+        print 'warning - this may take a while for n = ', n
+    g = igraph.Graph(n)
+    # clear graph
+    count_good = 0
+    count_bad = 0
+    while True:
+        break
+        # check graph - is it good?
+        #  find max clique # mc
+        mc = g.omega()
+        if mc >= q1: # then good graph
+            good += 1
+        else:
+            #  find max independent set # mi
+            mi = g.alpha()
+            if mi >= q2: # then good graph
+                good += 1
+            else: #  bad graph!
+                bad += 1
+        # increment graph
+        igraph.
+
+        # if graph is the first graph, break
+
+    assert count_good + count_bad == 2 ** (n*(n-1)/2)
+    return count_good / count_bad
+
+# edge 5-4 5-3 5-2 5-1
+
+def generate_graphs(n):
+    g = Graph(n)
+    for gg in generate_graphs_help(g,n-1,n-2):
+        yield gg
+    return
+
+def generate_graphs_help(g,i,j):
+    if i==-1:
+        yield g
+    elif j==-1:
+        for gg in generate_graphs_help(g,i-1,i-2):
+            yield gg
+    else:
+        for gg in generate_graphs_help(g,i,j-1):
+            yield gg
+        g.add_edge(i,j)
+        for gg in generate_graphs_help(g,i,j-1):
+            yield gg
+        g.delete_edges(i,j)
+    return
+
+# Given n and sample size m, sample m graphs of size n, count how many are good and return the fraction
+def goodfraction_sample(n,q1,q2,m)
+    print 'todo'
+
+
